6d66a8100534c96cf05659326fe5b3b95e6d96b0
CariCard <112133987+CariCard@users.noreply.github.com>
2024-08-28 11:42:54 -0400

    Add files via upload
    Signed-off-by: CariCard <112133987+CariCard@users.noreply.github.com>

---
+++ fracturing.c
@@ -0,0 +1,59 @@
+/*
+Function for calculating width,Function for Calculating Height, 
+Function for Calculating Distance, Function for Calculating Area, 
+Function for Calculating Perimeter, Implementation Details
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+
+int width;
+int height;
+float distance;
+int area;
+int perimeter;
+
+
+//getting info to calculate width
+printf("What is the width?: \n");
+scanf("%d", &width);
+printf("The width is: %d", width);
+
+
+//getting info to calculate height
+printf("What is the height?: \n");
+scanf("%d", &height);
+printf("The height is: %d", height);
+
+//calculate area
+
+height*width= area
+printf("The area is: %d", area);
+
+//calculate distance
+printf("What is the distance?: \n");
+
+printf("Input x1: \n");
+scanf("%d", &x1);
+
+printf("Input x2: \n");
+scanf("%d", &x2);
+
+printf("Input y1: \n");
+scanf("%d", &y1);
+
+printf("Input y2: \n");
+scanf("%d", &y2);
+
+distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+printf("The distance is: %d", distance);
+
+//calculate perimeter
+ 2 * (height + width)=perimeter
+printf("The perimeter is: %d", perimeter);
+
+
+}
