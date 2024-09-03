19e0b487108632ee39667ad653f04682a50d0068
CariCard <112133987+CariCard@users.noreply.github.com>
2024-09-02 21:25:51 -0400

    Update fracturing.c
    went over pdf made most changes
    
    Signed-off-by: CariCard <112133987+CariCard@users.noreply.github.com>

--- fracturing.c
+++ fracturing.c
@@ -7,10 +7,10 @@ Function for Calculating Perimeter, Implementation Details
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
+int main();
 
-#define PI = 3.14;
+
+#define PI = 3.14; // circle shape
  
 double calculate_width;
 double calculate_height;
@@ -18,43 +18,69 @@ float distance;
 double calculate_area;
 double calculae_perimeter;
 
-
-//getting info to calculate width
-printf("What is the width?: \n");
-scanf("%d", &width);
-printf("The width of the city encompassed by your request is: %d", width);
-
-
-//getting info to calculate height
-printf("What is the height?: \n");
-scanf("%d", &height);
-printf("The height of the city encompassed by your request is: %d", height);
-
-//calculate area
-height*width= area
-printf("The area of the city encompassed by your request is: %d", area);
+int main() // needs two arguments 
+{
+return 0;
+}
 
 //calculate distance
+void double calculate_distance()// no arguments
+{
 printf("What is the distance?: \n");
-
-printf("Input x1: \n");
+printf("Enter x1: \n");
 scanf("%d", &x1);
-
-printf("Input x2: \n");
+printf("Enter x2: \n");
 scanf("%d", &x2);
-
-printf("Input y1: \n");
+printf("Enter y1: \n");
 scanf("%d", &y1);
-
-printf("Input y2: \n");
+printf("Enter y2: \n");
 scanf("%d", &y2);
 
 distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+printf("Point #1 entered x1 = - ", x1; "y1 = - ", y1);
+printf("Point #2 entered x2 = - ", x2; "y2 = - ", y2);
 printf("The distance between two points is: %d", distance);
+// return 
+}
 
 //calculate perimeter
+void double calculate_perimeter()
+{
  2 * (height + width)=perimeter
+printf("Point #1 entered x1 = - ", x1; "y1 = - ", y1);
+printf("Point #2 entered x2 = - ", x2; "y2 = - ", y2);
 printf("The perimeter of the city encompassed by your request is: %d", perimeter);
+//return scale 1-5?
+}
 
-return 0;
+//calculate area
+void double calculate_area()
+{
+area = PI * radius * radius;
+printf("Point #1 entered x1 = - ", x1; "y1 = - ", y1);
+printf("Point #2 entered x2 = - ", x2; "y2 = - ", y2);
+printf("The area of the city encompassed by your request is: %d", area);
+//return scale 1-5?
+}
+
+//getting info to calculate width
+void double calculate_width()
+{
+printf("What is the width?: \n");
+scanf("%d", &width);
+printf("Point #1 entered x1 = - ", x1; "y1 = - ", y1);
+printf("Point #2 entered x2 = - ", x2; "y2 = - ", y2);
+printf("The width of the city encompassed by your request is: %d", width);
+ //return
+}
+
+//getting info to calculate height
+void double calculate_height()
+{
+printf("What is the height?: \n");
+scanf("%d", &height);
+printf("Point #1 entered x1 = - ", x1; "y1 = - ", y1);
+printf("Point #2 entered x2 = - ", x2; "y2 = - ", y2);
+printf("The height of the city encompassed by your request is: %d", height);
+ //return
 }
